c2242d24e910f1b22244822df5a2dbb9887714a6
amrmahmoud01 <amrsallam2001@gmail.com>
2026-02-20 22:53:17 +0200

    added .env to gitignore

--- spider20/spider20/spiders/configTest.py
+++ spider20/spider20/spiders/configTest.py
@@ -1,9 +1,9 @@
-import json
-with open("configs/orConfig.json") as f:
-    x = json.load(f)
+# import json
+# with open("configs/orConfig.json") as f:
+#     x = json.load(f)
 
-    for category, info in x.items():
-        for url in info["urls"]:
-            print(url["url"])
-            print(url["gender"])
+#     for category, info in x.items():
+#         for url in info["urls"]:
+#             print(url["url"])
+#             print(url["gender"])
 
