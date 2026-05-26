db4cf69141846f316125ba7d8794865fe48cd7e4
amrmahmoud01 <amrsallam2001@gmail.com>
2026-05-26 20:49:26 +0300

    updated iys dowload delay

--- spider20/spider20/spiders/iys.py
+++ spider20/spider20/spiders/iys.py
@@ -16,7 +16,7 @@ class IysSpider(scrapy.Spider):
         },
         "CONCURRENT_REQUESTS": 4,
         "CONCURRENT_REQUESTS_PER_DOMAIN": 4,
-        "DOWNLOAD_DELAY": 1,
+        "DOWNLOAD_DELAY": 1.5,
         "RANDOMIZE_DOWNLOAD_DELAY": True,
         "DEFAULT_REQUEST_HEADERS": {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0 Safari/537.36",
