bb71e745d6dba2ef7587ead5781f08bcfebffa4a
amrmahmoud01 <amrsallam2001@gmail.com>
2026-05-28 04:19:07 +0300

    enabled cookies

--- spider20/spider20/spiders/iys.py
+++ spider20/spider20/spiders/iys.py
@@ -22,6 +22,7 @@ class IysSpider(scrapy.Spider):
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0 Safari/537.36",
             "Accept-Language": "ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7",
         },
+        "COOKIES_ENABLED": True
 
                 }
     item = SpiderItem()
