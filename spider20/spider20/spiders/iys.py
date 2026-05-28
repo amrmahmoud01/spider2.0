aae5ba56b67b5e7e31e797e367692933768c5456
amrmahmoud01 <amrsallam2001@gmail.com>
2026-05-28 04:21:06 +0300

    sec or sth

--- spider20/spider20/spiders/iys.py
+++ spider20/spider20/spiders/iys.py
@@ -11,6 +11,18 @@ class IysSpider(scrapy.Spider):
     name = "iys"
 
     custom_settings = {
+        "DEFAULT_REQUEST_HEADERS": {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        "Accept-Language": "ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-User": "?1",
+        },
         "ITEM_PIPELINES": {
             "spider20.pipelines.SpiderPipeline": 300,
         },
