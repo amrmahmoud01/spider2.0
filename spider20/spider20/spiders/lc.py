37b868aae51dc2787ae2392bfa19d3d4a03454bd
amrmahmoud01 <amrsallam2001@gmail.com>
2026-07-13 12:11:58 +0300

    remove zyte api integration from LC waikiki spider

--- spider20/spider20/spiders/lc.py
+++ spider20/spider20/spiders/lc.py
@@ -22,9 +22,9 @@ class LCSpider(scrapy.Spider):
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0 Safari/537.36",
                 "Accept-Language": "en-US,en;q=0.9",
             },
-        "ADDONS": {
-            scrapy_zyte_api.Addon: 500,
-        }
+        # "ADDONS": {
+        #     scrapy_zyte_api.Addon: 500,
+        # }
                 }
         
 
